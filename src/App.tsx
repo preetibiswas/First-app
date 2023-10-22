d1f3ee7055b91aef84468ad63b87681060f0545c
Preeti biswas <preeti.biswas@brovitech.com>
2023-10-23 00:08:24 +0530

    my first-react-app-with-mosh

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Message from './Message'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Message/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Message.tsx
@@ -0,0 +1,4 @@
+function Message(){
+    return <h1>Hello Preeti</h1>
+}
+export default Message
